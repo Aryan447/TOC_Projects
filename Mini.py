41fa37e26e9ac5ca1a2468acf081e4fe41221fe0
Aryan447 <89701129+Aryan447@users.noreply.github.com>
2023-11-21 19:24:35 +0530

    code fixed

--- Mini.py
+++ Mini.py
@@ -1,42 +1,52 @@
 def turing_machine(tape):
+    tape = list(tape)  # Convert the input string to a list of characters
     tape_head = 0
+    state = 'q0'
 
-    while tape_head < len(tape):
+    while True:
         current_symbol = tape[tape_head]
 
-        if current_symbol == 'a':
-            tape[tape_head] = 'X'
-            tape_head += 1
-            state = 'q1'
-        elif current_symbol == 'X':
-            tape_head += 1
-            state = 'q1'
-        elif current_symbol == 'b':
-            tape[tape_head] = 'Y'
-            tape_head += 1
-            state = 'q2'
-        elif current_symbol == 'Y':
-            tape_head += 1
-            state = 'q2'
-        elif current_symbol == 'c':
-            tape[tape_head] = 'Z'
-            tape_head += 1
-            state = 'q3'
-        elif current_symbol == 'Z':
-            tape_head += 1
-            state = 'q3'
-        elif current_symbol == '$':
-            break
-        else:
-            return False
+        if state == 'q0':
+            if current_symbol == 'a':
+                tape[tape_head] = 'X'
+                tape_head += 1
+            elif current_symbol == 'b':
+                tape[tape_head] = 'Y'
+                tape_head += 1
+                state = 'q1'
+            elif current_symbol == '$':
+                break
+            else:
+                return False
+        elif state == 'q1':
+            if current_symbol == 'b':
+                tape_head += 1
+            elif current_symbol == 'c':
+                tape[tape_head] = 'Z'
+                tape_head += 1
+                state = 'q2'
+            else:
+                return False
+        elif state == 'q2':
+            if current_symbol == 'c':
+                tape_head += 1
+            elif current_symbol == 'X':
+                tape[tape_head] = 'a'
+                tape_head -= 1
+                state = 'q0'
+            elif current_symbol == 'Y':
+                tape[tape_head] = 'b'
+                tape_head -= 1
+            elif current_symbol == 'Z':
+                tape[tape_head] = 'c'
+                tape_head -= 1
+            else:
+                return False
 
-    if state == 'q3':
-        return True
-    else:
-        return False
+    return state == 'q0' and tape[tape_head] == '$'
 
 if __name__ == '__main__':
-    tape = input('Enter the tape string: ')
+    tape = input('Enter the tape string: ') + '$'
     result = turing_machine(tape)
 
     if result:
